bc1ba761c8ba5cd87d36b863fab07f3882fda755
Douglas Muth <dmuth@dmuth.org>
2019-11-17 19:54:21 -0500

    Wrote scripts to build a hierarchy of test containers and then run the script for testing.

---
+++ build-test-containers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Build our test docker containres
+#
+
+# Errors are fatal
+set -e
+
+docker build -t test_main .
+
+docker build -t test_main_child_1 -f ./Dockerfile-child-1 .
+docker build -t test_main_child_2 -f ./Dockerfile-child-1 .
+docker build -t test_main_child_3 -f ./Dockerfile-child-1 .
+
+docker build -t test_main_child_1_1 -f ./Dockerfile-child-1-1 .
+docker build -t test_main_child_1_2 -f ./Dockerfile-child-1-1 .
+
+IMAGE=$(docker images | grep "test_main " | awk '{print $3}')
+
+echo "# "
+echo "# The Main Docker image is: ${IMAGE}"
+echo "# "
+
+echo "# Now removing that image..."
+
+./docker-remove-image ${IMAGE}
+
+echo "# Test complete!"
+
